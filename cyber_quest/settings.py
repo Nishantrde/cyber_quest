8d3ab93c580e73e3e79831491b82cd9763485465
Nishant <nishant2024rde@gmail.com>
2024-10-02 00:27:55 -0700

    Initial Commit

--- cyber_quest/settings.py
+++ cyber_quest/settings.py
@@ -129,10 +129,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Cloudinary for static files storage
 # STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticCloudinaryStorage'
